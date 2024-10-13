1195d9467aaceeaa31913ffd514e6bb6dca7e9a6
Pavel <pavelgren105@outlook.com>
2024-10-13 22:05:01 +0300

    init

---
+++ app/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(libs.junit.jupiter)
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation(libs.guava)
+    implementation("io.github.neodix42:smartcontract:0.7.0")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+application {
+    mainClass = "org.example.App"
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
